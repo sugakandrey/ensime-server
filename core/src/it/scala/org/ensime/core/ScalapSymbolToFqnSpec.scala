45e441dcb9c6750d636899646c3e630e5e213b7d
Sugak Andrey <2th3sk13s@gmail.com>
2016-06-11 16:11:11 +0400

    Very basic tests for ScalapSymbolToFqn

---
+++ core/src/it/scala/org/ensime/core/ScalapSymbolToFqnSpec.scala
@@ -0,0 +1,44 @@
+package org.ensime.core
+
+import org.ensime.api.EnsimeConfig
+import org.ensime.fixture.{ EnsimeConfigFixture, IsolatedRichPresentationCompilerFixture }
+import org.ensime.indexer._
+import org.ensime.util.EnsimeSpec
+import org.ensime.vfs._
+
+class ScalapSymbolToFqnSpec extends EnsimeSpec
+    with IsolatedRichPresentationCompilerFixture
+    with RichPresentationCompilerTestUtils
+    with ReallyRichPresentationCompilerFixture
+    with ScalapSymbolToFqn {
+
+  override def original: EnsimeConfig = EnsimeConfigFixture.ShapelessTestProject
+
+  "ScalapSymbolToFqn" should "find and resolve class names defined in Predef" in withPresCompiler { (_, cc) =>
+    val vfs = cc.vfs
+
+    val predef = vfs.vres("scala/Predef.class")
+    val definedClassNames = new ClassfileDepickler(predef).getClasses
+    definedClassNames.length should ===(22)
+    /* definedClassNames.foreach { className =>
+      val sym = cc.askSymbolByFqn(className)
+      sym shouldBe defined
+      sym.get should not be an[cc.NoSymbol]
+    }
+        This is currently broken, see
+        https://github.com/ensime/ensime-server/issues/1507 */
+  }
+
+  it should "find and resolve field names in Predef" in withPresCompiler { (_, cc) =>
+    val vfs = cc.vfs
+    val search = cc.search
+
+    val predef = vfs.vres("scala/Predef.class")
+    val fieldNames = new ClassfileDepickler(predef).getFields
+    fieldNames.foreach { field =>
+      val sym = cc.askSymbolByFqn(field)
+      sym shouldBe defined
+      sym.get should not be an[cc.NoSymbol]
+    }
+  }
+}
