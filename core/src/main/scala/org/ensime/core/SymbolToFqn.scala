ea70f76a10e98a9023a5f5346978775047da2a33
Sugak Andrey <2th3sk13s@gmail.com>
2016-07-02 16:00:14 +0400

    revert redundant changes

--- core/src/main/scala/org/ensime/core/SymbolToFqn.scala
+++ core/src/main/scala/org/ensime/core/SymbolToFqn.scala
@@ -23,7 +23,22 @@ import org.slf4j.Logger
  */
 trait SymbolToFqn { self: Global with PresentationCompilerBackCompat =>
   def logger: Logger
-  import FqnUtils._
+
+  import ClassName._
+  val ScalaPackageName: PackageName = PackageName(List("scala"))
+  val normaliseClass: ClassName => ClassName = Map(
+    ClassName(PackageName(List("scala", "runtime")), "BoxedUnit") -> PrimitiveVoid,
+    ClassName(ScalaPackageName, "<byname>") -> ClassName(ScalaPackageName, "Function0"),
+    ClassName(ScalaPackageName, "Boolean") -> PrimitiveBoolean,
+    ClassName(ScalaPackageName, "Byte") -> PrimitiveByte,
+    ClassName(ScalaPackageName, "Char") -> PrimitiveChar,
+    ClassName(ScalaPackageName, "Short") -> PrimitiveShort,
+    ClassName(ScalaPackageName, "Int") -> PrimitiveInt,
+    ClassName(ScalaPackageName, "Long") -> PrimitiveLong,
+    ClassName(ScalaPackageName, "Float") -> PrimitiveFloat,
+    ClassName(ScalaPackageName, "Double") -> PrimitiveDouble,
+    ClassName(ScalaPackageName, "Void") -> PrimitiveVoid
+  ).withDefault(identity)
 
   private def packageName(sym: Symbol): PackageName = {
     PackageName(sym.ownerChain.takeWhile(!_.isRootSymbol).reverse.map(_.encodedName))
