18a717e81d3b8c3a1106f403501bb949d39e74da
Sugak Andrey <sugak.andr3y@gmail.com>
2016-10-07 13:56:22 +0300

    remove tests with disabled reveseLookups

--- core/src/it/scala/org/ensime/intg/ReverseLookupsSpec.scala
+++ core/src/it/scala/org/ensime/intg/ReverseLookupsSpec.scala
@@ -9,8 +9,6 @@ import org.ensime.util.EnsimeSpec
 import org.ensime.util.ensimefile.Implicits.DefaultCharset
 import org.ensime.util.file._
 
-import scala.util.Properties
-
 class ReverseLookupsSpec extends EnsimeSpec
     with IsolatedEnsimeConfigFixture
     with IsolatedTestKitFixture
@@ -37,28 +35,6 @@ class ReverseLookupsSpec extends EnsimeSpec
         )
       }
     }
-  }
-
-  it should "only search files loaded into pres. compiler when reverse lookups are turned off" in {
-    try {
-      Properties.setProp("ensime.index.no.reverse.lookups", "true")
-      withEnsimeConfig { implicit config =>
-        withTestKit { implicit testKit =>
-          withProject { (project, asyncHelper) =>
-            import testKit._
-            val sourceRoot = scalaMain(config)
-            val fooFile = sourceRoot / "org/example/Foo.scala"
-
-            project ! UsesOfSymbolAtPointReq(Left(fooFile), 119)
-            val uses = expectMsgType[ERangePositions]
-            uses.positions.map(usage => (s"${File(usage.file).getName}", usage.offset, usage.start, usage.end)) should contain theSameElementsAs List(
-              ("Foo.scala", 114, 110, 172),
-              ("Foo.scala", 273, 269, 283)
-            )
-          }
-        }
-      }
-    } finally Properties.clearProp("ensime.index.no.reverse.lookups")
   }
 
   "Refactor Rename" should "make use of reverse lookups information" in withEnsimeConfig { implicit config =>
@@ -102,43 +78,5 @@ class ReverseLookupsSpec extends EnsimeSpec
         }
       }
     }
-  }
-
-  it should "only search files loaded into pres. compiler when reverse lookups are turned off" in {
-    try {
-      Properties.setProp("ensime.index.no.reverse.lookups", "true")
-      withEnsimeConfig { implicit config =>
-        withTestKit { implicit testKit =>
-          withProject { (project, asyncHelper) =>
-            import testKit._
-            val sourceRoot = scalaMain(config)
-            val fooFile = sourceRoot / "org/example/Foo.scala"
-
-            project ! RefactorReq(1234, RenameRefactorDesc("notATestMethod", fooFile, 119, 119), interactive = false)
-            expectMsgPF() {
-              case response @ RefactorDiffEffect(1234, RefactorType.Rename, diff) =>
-                val relevantExpectedPartFoo =
-                  s"""|@@ -9,3 +9,3 @@
-                      |   class Foo extends Bar {
-                      |-    def testMethod(i: Int, s: String) = {
-                      |+    def notATestMethod(i: Int, s: String) = {
-                      |       i + s.length
-                      |@@ -16,3 +16,3 @@
-                      |   println("Hello, " + foo.x)
-                      |-  println(foo.testMethod(7, "seven"))
-                      |+  println(foo.notATestMethod(7, "seven"))
-                      | 
-                      |""".stripMargin
-
-                val diffContents = diff.canon.readString()
-                val expectedContents = expectedDiffContent(fooFile.getPath, relevantExpectedPartFoo)
-
-                if (diffContents == expectedContents) true
-                else fail(s"Different diff content than expected. \n Actual content: '$diffContents' \n ExpectedRelevantContent: '$expectedContents'")
-            }
-          }
-        }
-      }
-    } finally Properties.clearProp("ensime.index.no.reverse.lookups")
   }
 }
