3eb97829d0d270a61a1ec52ec8880747e27f33ba
Sugak Andrey <sugak.andr3y@gmail.com>
2016-12-15 16:34:33 +0300

    fix versioning

--- project/EnsimeBuild.scala
+++ project/EnsimeBuild.scala
@@ -25,7 +25,7 @@ object ProjectPlugin extends AutoPlugin {
   override def buildSettings = Seq(
     scalaVersion := "2.11.8",
     organization := "org.ensime",
-    version := "2.0.0-graph-SNAPSHOT",
+    version := "2.0.0-SNAPSHOT",
 
     // WORKAROUND: https://github.com/fommil/sbt-sensible/issues/6
     crossPaths := true,
