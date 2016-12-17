8c663d30d43da4f275dd86d51d6ed7d4d93e3a91
Sugak Andrey <sugak.andr3y@gmail.com>
2016-12-17 20:07:26 +0300

    tags to enable CI-env-specific test ignores

---
+++ testutil/src/main/scala/org/ensime/util/IgnoreOnTravis.java
@@ -0,0 +1,13 @@
+// Copyright: 2010 - 2016 https://github.com/ensime/ensime-server/graphs
+// License: http://www.gnu.org/licenses/gpl-3.0.en.html
+package org.ensime.util;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.ElementType;
+
+@org.scalatest.TagAnnotation
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+public @interface IgnoreOnTravis {}
---
+++ testutil/src/main/scala/org/ensime/util/tags/IgnoreOnTravis.scala
@@ -0,0 +1,7 @@
+// Copyright: 2010 - 2016 https://github.com/ensime/ensime-server/graphs
+// License: http://www.gnu.org/licenses/gpl-3.0.en.html
+package org.ensime.util.tags
+
+import org.scalatest.Tag
+
+object IgnoreOnTravis extends Tag("org.ensime.util.IgnoreOnTravis")
